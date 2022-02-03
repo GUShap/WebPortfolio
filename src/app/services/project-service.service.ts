0e22d2bf42acf0c90d2fdb66d962633865687e8f
GUShap <shapira91@gmail.com>
2022-02-04 00:14:10 +0200

    img url fix

--- src/app/services/project-service.service.ts
+++ src/app/services/project-service.service.ts
@@ -22,11 +22,11 @@ export class ProjectService {
   constructor(private utils:UtilsService) { }
 
 projectData:Project[]=[
-  new Project('Stocks Dashboard',this.utils.setId(),'/assets/images/stock-dashboard.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Stocks-Dashboard-Angular/'),
-  new Project('Tasker',this.utils.setId(),'/assets/images/tasker.jpg',this.lorem.generateSentences(4),'https://tasker-app-2021.herokuapp.com/#/board'),
-  new Project('dragNote',this.utils.setId(),'/assets/images/dragNote.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/DragNote-Angular/'),
-  new Project('Packman!',this.utils.setId(),'/assets/images/packman.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Pacman/'),
-  new Project('Take the Weather',this.utils.setId(),'/assets/images/weather.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Weather-App-Angular/')
+  new Project('Stocks Dashboard',this.utils.setId(),'./assets/images/stock-dashboard.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Stocks-Dashboard-Angular/'),
+  new Project('Tasker',this.utils.setId(),'./assets/images/tasker.jpg',this.lorem.generateSentences(4),'https://tasker-app-2021.herokuapp.com/#/board'),
+  new Project('dragNote',this.utils.setId(),'./assets/images/dragNote.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/DragNote-Angular/'),
+  new Project('Packman!',this.utils.setId(),'./assets/images/packman.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Pacman/'),
+  new Project('Take the Weather',this.utils.setId(),'./assets/images/weather.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Weather-App-Angular/')
 ];
 }
 
