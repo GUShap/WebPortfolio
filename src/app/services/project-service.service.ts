87261fc0a1a8af0b1b592458d910da82fffc5f08
GUShap <shapira91@gmail.com>
2022-02-09 16:38:01 +0200

    !

--- src/app/services/project-service.service.ts
+++ src/app/services/project-service.service.ts
@@ -19,14 +19,44 @@ export class ProjectService {
    }
   })
 
+
   constructor(private utils:UtilsService) { }
 
 projectData:Project[]=[
-  new Project('Stocks Dashboard',this.utils.setId(),'./assets/images/stock-dashboard.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Stocks-Dashboard-Angular/'),
-  new Project('Tasker',this.utils.setId(),'./assets/images/tasker.jpg',this.lorem.generateSentences(4),'https://tasker-app-2021.herokuapp.com/#/board'),
-  new Project('dragNote',this.utils.setId(),'./assets/images/dragNote.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/DragNote-Angular/'),
-  new Project('Packman!',this.utils.setId(),'./assets/images/packman.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Pacman/'),
-  new Project('Take the Weather',this.utils.setId(),'./assets/images/weather.jpg',this.lorem.generateSentences(4),'https://gushap.github.io/Weather-App-Angular/')
+  {name:'Stocks Dashboard',
+    id:this.utils.setId(),
+    imgUrl:'./assets/images/stock-dashboard.jpg',
+    description:'My first "Big" Angular project, inspired by task from Coding Academy. You will be able to set a portfolio of stocks you wish to follow, using restAPI with data from AlphaVentage and including much of angular abilities, like: RxJS, HTTPClient, angular-material and much more',
+    link:'https://gushap.github.io/Stocks-Dashboard-Angular/'
+  },
+  {
+    name: 'Tasker',
+    id:this.utils.setId(),
+    imgUrl: './assets/images/tasker.jpg',
+    description:'The Final Project in my training at Coding Academy. A "Monday" like app for managing projects and teams. My first time working as a team and i enjoyed every moment of it',
+    link: 'https://tasker-app-2021.herokuapp.com/#/board'
+  },
+  {
+    name: 'dragNote',
+    id:this.utils.setId(),
+    imgUrl: './assets/images/dragNote.jpg',
+    description:'Discovering my passion to Angular framework and experimenting with some of what it can offer, in this case mainly BEAUTIFUL DRAG&DROP.',
+    link:'https://gushap.github.io/DragNote-Angular/'
+  },
+  {
+    name:'Packman!',
+    id: this.utils.setId(),
+    imgUrl:'./assets/images/packman.jpg',
+    description:'The good old yellow circled friend, given as a task at the very beginning. Very nostalgic and shows one of the first "serious" css styling i made.',
+    link:'https://gushap.github.io/Pacman/'
+  },
+  {
+    name:'Take the Weather',
+    id: this.utils.setId(),
+    imgUrl:'./assets/images/weather.jpg',
+    description:'A weather app, that very basically shows to forecast for the next few days in any place the user inputs. Using restAPI with data from AccuWeather',
+    link:'https://gushap.github.io/Weather-App-Angular/'
+  },
 ];
 }
 
